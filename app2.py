c5bf34d6e4f9a507f9acb9a5da526818b37e9b5e
stan05 <stanislav.petr7@gmail.com>
2020-04-12 04:48:52 +0200

    rebuilding whole app

--- app2.py
+++ app2.py
@@ -7,14 +7,9 @@ from io import BytesIO
 import mysql.connector
 import os
 from dotenv import load_dotenv
-import gui
-
+from gui import GermanGame
 
 load_dotenv()
-"""
-main functionality in SQL
-1.
-"""
 
 USERNAME = os.getenv("USER_NAME")
 PASSWD = os.getenv("PASSWD")
@@ -28,8 +23,82 @@ db = mysql.connector.connect(
 )
 
 db_cursor = db.cursor()
+db_cursor.execute("select * from phrases")
 
-now = datetime.date.today()
+all_phrases = db_cursor.fetchall() #(98, 'Sein Geist', None, 0)
+phrases_counter = 1          #tracks how many phrases left, set on 1 because of excel start position on 2
+anti_repeat = False          #prevents yes/no button spamming
+
+
+def pick_a_phrase(self):
+    global all_phrases, phrases_counter, anti_repeat
+    unformatted_now = datetime.datetime.now()
+    
+    now_str = str(unformatted_now).split(".")[0]
+    now_datetime = datetime.datetime.strptime(now_str, "%Y-%m-%d %H:%M:%S")
+    
+    for index, phrase in enumerate(all_phrases):
+        phrase_id = phrase[0]
+        phrase_text = phrase[1]
+        phrase_date = phrase[2]
+        phrase_frequency = phrase[3]
+
+        if phrase_text:        
+            if not phrase_date:
+                sql = "update phrases set date = %s and frequency=1 where id = %s"
+                val = (now_str, phrase_id)
+
+                #db_cursor.execute(sql, val)  #if the cell doesnt have any date puts today's date
+                phrases_counter += 1
+            else:
+                date_difference = now_datetime - phrase_date
+                # #retrieve date
+                # #date gets passed from sql database as int()
+                # db_cursor.execute("select date from phrases where id=4")
+                # date = db_cursor.fetchall()[0][0]
+                # #this change it to datetime.datetime class
+                # cur_date = datetime.datetime.strptime(str(date), "%Y-%m-%d %H:%M:%S")
+
+                days_difference = date_difference.days #count how many days passed from the last practise
+
+                phrases_counter += 1
+                if days_difference >= phrase_frequency:  #checks frequency of practising the phrase
+                    sql = "update phrases set date = %s where id = %s"
+                    val = (now, phrase_id)
+
+                    phrase("check here ELSE")
+                    # db_cursor.execute(sql, val)  #if the cell doesnt have any date puts today's date
+                    phrases_counter += 1
+                    print('pass' + str(phrase_id))
+                    pass     #pass to the next stage
+                else:
+                    print('continue' + str(phrase_id))
+                    continue
+
+            GermanGame.pick_phrase_gui(self, phrase_text, yes_funct, no_funct)
+
+            all_phrases.pop(index)
+            break
+
+        else:
+            print(f"skipped {phrase_id}")
+            phrases_counter += 1
+            continue
+
+def yes_funct():
+    print("works")
+
+def no_funct():
+    print("works2")
+
+root = tk.Tk()
+
+GermanGame(root, pick_a_phrase)
+
+root.mainloop()
+
+
+db_cursor = db.cursor()
 
 db_cursor.execute("select max(id) from phrases")
 max_id = db_cursor.fetchall()[0][0] #to fetch data and be able to retrieve them -> ([98, ]) -> [0][0] -> 98
@@ -45,13 +114,6 @@ not_id = db_cursor.fetchall()
 db_cursor.execute("select * from phrases where id=4")
 not_phrase = db_cursor.fetchall()
 
-db_cursor.execute("select * from phrases")
-all_phrases = db_cursor.fetchall() #(98, 'Sein Geist', None, 0)
-
-used_phrase_id = []          #used to prevent in repeating the same phrases
-phrases_counter = 1          #tracks how many phrases left, set on 1 because of excel start position on 2
-anti_repeat = False          #prevents yes/no button spamming
-
 #retrieve date
 #date gets passed from sql database as int()
 db_cursor.execute("select date from phrases where id=4")
@@ -60,62 +122,11 @@ date = db_cursor.fetchall()[0][0]
 cur_date = datetime.datetime.strptime(str(date), "%Y-%m-%d %H:%M:%S")
 
 #pass date
-formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+unformatted_now = datetime.date.today()
+now = unformatted_now.strftime('%Y-%m-%d %H:%M:%S')
 
 sql = "UPDATE phrases SET date = %s WHERE id = %s"
-val = (formatted_date, 5)
+val = (now, 5)
 
 db_cursor.execute(sql, val)
 db_cursor.execute("select date from phrases where id=5")
-#########################################################
-
-# print(a)
-
-# cur_date = datetime.datetime.strptime(now, "%Y-%m-%d %H:%M:%S")
-
-# db_cursor.execute(f"update phrases set date=' where id=5")
-# db_cursor.execute("select * from phrases where id=5"
-
-
-
-
-
-# def pick_a_phrase(none):
-#     global anti_repeat, phrases_counter
-
-#     for index, phrase in enumerate(all_phrases):
-#         phrase_id = phrase[0]
-#         phrase_text = phrase[1]
-#         phrase_date = phrase[2]
-#         phrase_frequency = phrase[3]
-
-#         if phrase_text:        
-#             if phrase_date None:
-#                 db_cursor.execute("update phrases set frequency=1 where id={}".format(phrase_id))
-#                 work_sheet.cell(row=n, column=3).value = now  #if the cell doesnt have any date puts today's date
-#                 phrases_counter += 1
-#             else:
-#                 datum_counter_minor = now - datetime.datetime.date(work_sheet.cell(row=n, column=3).value)
-#                 datum_counter_major = datum_counter_minor.days
-#                 phrases_counter += 1
-#                 if datum_counter_major >= work_sheet.cell(row=n, column=5).value:  #checks frequency of practising the phrase
-#                     work_sheet.cell(row=n, column=3).value = now
-#                     print('pass' + str(n))
-#                     pass
-#                 else:
-#                     print('continue' + str(n))
-#                     continue
-
-#             wb.save(file_name)
-#             break
-#         else:
-#             print(f"skipped {phrase_id}")
-#             phrases_counter += 1
-#             continue
-
-# root = tk.Tk()
-
-# App = gui.GermanGame(root, current_phrase)
-
-
-# root.mainloop()
--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ import os
 from dotenv import load_dotenv
 
 
-
+"""pictures"""
 img_yes_url = requests.get('https://www.derjogger.de/wp-content/uploads/2016/03/Zitat-zu-Ausdauer-Aussehen-und-Sport.png')
 img_yes = Image.open(BytesIO(img_yes_url.content))
 img_yes.thumbnail((350, 350), Image.ANTIALIAS)
@@ -24,9 +24,9 @@ img_bg= Image.open(BytesIO(img_bg_url.content))
 
 class GermanGame:
     #init Gui fro the app
-    def __init__(self, root, data):
-        self.root = root
-        self.data = data
+    def __init__(self, root, pick_phrase_func):
+        # self.root = root
+        self.main_func = pick_phrase_func
         
         root.geometry('1000x500')
         root.configure(background='cyan')
@@ -39,44 +39,46 @@ class GermanGame:
 
         button_pick_a_phrase = tk.Button(text='Pick a phrase', bg='gold', font=(15))
         button_pick_a_phrase.place(x=220, y=150)
-        button_pick_a_phrase.bind('<Button-1>', self.pick_phrase_gui)
+        button_pick_a_phrase.bind('<Button-1>', self.main_func)
 
         label_version = tk.Label(text='Version 0.2', font=('arial 10 bold'))
         label_version.place(x=900, y=450)
 
         label_lets_start = tk.Label(master=root, text='LET\'S START', font=('Helvetica', 60, 'bold'), bg='gold')
         label_lets_start.place(x=30, y=10)
+
+        root.mainloop
         
     #gui after clicking on PICK_A_PHRASE button
-    def pick_phrase_gui(self, *args, **kwargs):
-        text_phrase = tk.Text(master=self.root, width=40, height=5, bg='light goldenrod')
+    def pick_phrase_gui(self, current_phrase, yes, no):
+        text_phrase = tk.Text(width=40, height=5, bg='light goldenrod')
         text_phrase.place(x=120, y=190)
-        text_phrase.insert(tk.END, "this is my text")
+        text_phrase.insert(tk.END, current_phrase)
 
-        button_yes = tk.Button(master=self.root, text='Yes', font=(15), bg='green', command=yes_button)
+        button_yes = tk.Button(text='Yes', font=(15), bg='green', command=yes)
         button_yes.place(x=220, y=290)
         #yes_button.bind('<Up>', yes_button)
 
-        button_no = tk.Button(master=self.root, text='No', font=(15), bg='red', command=no_button)
+        button_no = tk.Button(text='No', font=(15), bg='red', command=no)
         button_no.place(x=300, y=290)
         #no_button.bind('<Button-Down>', no_button)
 
-        button_delete_phrase = tk.Button(master=self.root, text='delete phrase', font=(10), command=delete_phrase)
-        button_delete_phrase.place(x=750, y=450)
+        # button_delete_phrase = tk.Button(master=self.root, text='delete phrase', font=(10), command=delete_phrase)
+        # button_delete_phrase.place(x=750, y=450)
 
-        button_add = tk.Button(master=self.root, text='add phrase', font=(10), command=add_phrase)
-        button_add.place(x=650, y=450)
+        # button_add = tk.Button(master=self.root, text='add phrase', font=(10), command=add_phrase)
+        # button_add.place(x=650, y=450)
 
-        add_text = tk.Entry(master=self.root)            #not finished yet
-        add_text.place(x=480, y=450)
+        # add_text = tk.Entry(master=self.root)            #not finished yet
+        # add_text.place(x=480, y=450)
 
-        label_score = tk.Label(master=self.root, text='Score', bg='gold')
+        label_score = tk.Label(text='Score', bg='gold')
         label_score.place(x=840, y=30)
 
-        text_yes_score = tk.Text(master=self.root, width=4, heigh=1, bg='green', font=('bold'))
+        text_yes_score = tk.Text(width=4, heigh=1, bg='green', font=('bold'))
         text_yes_score.place(x=790, y=30)
-        text_yes_score.insert(tk.END, yes_score)
+        text_yes_score.insert(tk.END, "0")
 
-        text_no_score = tk.Text(master=self.root, width=4, height=1, bg='red', font=('bold'))
+        text_no_score = tk.Text(width=4, height=1, bg='red', font=('bold'))
         text_no_score.place(x=883, y=30)
-        text_no_score.insert(tk.END, no_score)
+        text_no_score.insert(tk.END, "0")
